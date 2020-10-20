195779840d24dda571240b1605d9e5d31f02ca3b
Miko <41611726+Miko2x@users.noreply.github.com>
2020-10-20 14:17:15 +0700

    Create file Styles.js
    Please add hacktoberfest-accepted tag 💯

---
+++ Styles.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  textHeader: {
+    fontWeight: 'bold',
+    fontSize: 35,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    width: width,
+    height: height,
+    alignItems: 'center',
+    opacity: 10,
+  },
+  TextInput: {
+    padding: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    height: 40,
+    borderWidth: 4,
+  },
+  pleaseFulfill: {
+    fontSize: 20,
+    marginTop: 150,
+  },
+  Button: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  Picker: {
+    height: 50,
+    width: 150,
+  },
+});
